bcb1271a26291a772a943783c1531cf459aab5e4
Maria Esther Cuan Cossio <esthercuan@Marias-MacBook-Pro.local>
2021-12-02 16:12:32 -0600

    first version of a bio CLI in node

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+console.log(`
+    🧸 Esther Cuan
+
+    An Engineering Manager, Software engineer and Developer advocate. 
+    Guiding engineers to success and leading amazing teams. 
+
+    🦋 Linked In: https://www.linkedin.com/in/esthercuan/
+    🐦 Twitter: https://twitter.com/esthercuan
+    📸 Photography: https://www.esthercuan.com/
+    🪴 Garden: https://ricehopper.co/
+
+`);
